35cc2f0ffb6193b3c907ca9c4adefbc558230f3b
lizhiyi123 <lizhiyi166@gmail.com>
2020-06-17 20:01:33 +0800

    eureka

--- eureka/src/main/java/com/example/goods/eureka/EurekaApplication.java
+++ eureka/src/main/java/com/example/goods/eureka/EurekaApplication.java
@@ -1,9 +1,11 @@
 package com.example.goods.eureka;
 
+import com.ctrip.framework.apollo.spring.annotation.EnableApolloConfig;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.netflix.eureka.server.EnableEurekaServer;
 
+@EnableApolloConfig
 @EnableEurekaServer
 @SpringBootApplication
 public class EurekaApplication {
