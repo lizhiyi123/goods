5ffb6a66d2e80e8ce52651893abb2f8e14ddf316
lizhiyi123 <lizhiyi166@gmail.com>
2020-06-17 18:33:29 +0800

    gateway

--- gateway/src/main/java/com/example/cloud/gateway/GatewayApplication.java
+++ gateway/src/main/java/com/example/cloud/gateway/GatewayApplication.java
@@ -3,9 +3,10 @@ package com.example.cloud.gateway;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 
 
-@EnableDiscoveryClient
+@EnableEurekaClient
 @SpringBootApplication
 public class GatewayApplication {
 
