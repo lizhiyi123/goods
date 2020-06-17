03e1703656ade37690ef5f157e7711778ceb2461
lizhiyi123 <lizhiyi166@gmail.com>
2020-06-17 20:45:26 +0800

    admin

--- admin/src/main/java/com/example/goods/admin/AdminApplication.java
+++ admin/src/main/java/com/example/goods/admin/AdminApplication.java
@@ -1,10 +1,12 @@
 package com.example.goods.admin;
 
+import com.ctrip.framework.apollo.spring.annotation.EnableApolloConfig;
 import de.codecentric.boot.admin.server.config.EnableAdminServer;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 
+@EnableApolloConfig
 @EnableEurekaClient
 @EnableAdminServer
 @SpringBootApplication
