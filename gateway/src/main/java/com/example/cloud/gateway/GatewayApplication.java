28279771aaf1f82bbaa0fead0c2f017507add869
lizhiyi123 <lizhiyi166@gmail.com>
2020-06-17 20:47:50 +0800

    gateway

--- gateway/src/main/java/com/example/cloud/gateway/GatewayApplication.java
+++ gateway/src/main/java/com/example/cloud/gateway/GatewayApplication.java
@@ -1,11 +1,13 @@
 package com.example.cloud.gateway;
 
+import com.ctrip.framework.apollo.spring.annotation.EnableApolloConfig;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 
 
+@EnableApolloConfig
 @EnableEurekaClient
 @SpringBootApplication
 public class GatewayApplication {
